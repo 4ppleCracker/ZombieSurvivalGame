564312f36b92f352a06ced4f7404705d16fbe586
4ppleCracker <4pplecracker@gmail.com>
2018-05-28 23:56:45 +0200

    Initial Commit

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Reflection;
+
+public class TileSpriteResource : Attribute
+{
+    public string location;
+    public TileSpriteResource(string location) {
+        this.location = location;
+    }
+}
+
+public enum TileType
+{
+    [TileSpriteResource("Sprites/Ground/Grass")]
+    Grass
+}
+
+public class Tile : MonoBehaviour {
+    public GameObject TileObject = null;
+    private Dictionary<string, Sprite> TileSprites = new Dictionary<string, Sprite>();
+    public TileType Type;
+    public string TypeName {
+        get {
+            return Enum.GetName(typeof(TileType), Type);
+        }
+    }
+    public Sprite GetSprite() {
+        return TileSprites[TypeName];
+    }
+    public void UpdateTile() {
+        gameObject.GetComponent<SpriteRenderer>().sprite = GetSprite();
+    }
+
+    private void Start() {
+        Type type = typeof(TileType);
+        foreach (TileType tiletype in Enum.GetValues(typeof(TileType))) {
+            string tiletypestr = tiletype.ToString();
+
+            MemberInfo[] memberInfos = type.GetMember(tiletypestr);
+            object[] attributes = memberInfos[0].GetCustomAttributes(false);
+            TileSpriteResource resource = (TileSpriteResource)attributes[0];
+
+            Sprite sprite = Resources.Load<Sprite>(resource.location);
+            TileSprites.Add(tiletypestr, sprite);
+        }
+    }
+}
---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public static TileManager singleton;
+
+    public List<List<GameObject>> Map = new List<List<GameObject>>();
+
+    public const int Width = 10;
+    public const int Height = 10;
+    public const float ppu = 2.5f;
+
+    private void InitMap() {
+        GameObject prefab = Resources.Load<GameObject>("Prefabs/Ground");
+        for (int x = 0; x < Width; x++) {
+            List<GameObject> mapstrip = new List<GameObject>();
+            for (int y = 0; y < Width; y++) {
+                GameObject gameObject = Instantiate(prefab);
+                Texture2D texture = prefab.GetComponent<SpriteRenderer>().sprite.texture;
+                gameObject.transform.position += new Vector3(x * ppu, 0, y * ppu);
+                mapstrip.Add(gameObject);
+            }
+            Map.Add(mapstrip);
+        }
+    }
+    private void SetCameraPosition() {
+        GameObject obj = GameObject.Find("Main Camera").GetComponent<Camera>().gameObject;
+        obj.transform.position = new Vector3((Width / 2) * ppu, 10, (Height / 2) * ppu);
+    }
+    private void Start() {
+        singleton = GetComponent<TileManager>();
+        InitMap();
+        SetCameraPosition();
+        Debug.Log("Made a grid of size " + Map.Count + "x" + (Map.Count > 0 ? Map[0].Count : 0));
+    }
+}
